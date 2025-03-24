1ee8b7481a2869bf7b82e16048a01646e1a7f04d
Daire Bohan <bohan.daire@gmail.com>
2025-03-23 21:29:58 -0700

    feat: export ReactiveList from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Computed } from './computed';
 import { Observer } from './observer';
 import { State } from './state';
+import { ReactiveList } from './reactive-list';
 
 export {
 	State,
 	Computed,
-	Observer
+	Observer,
+	ReactiveList
 }
